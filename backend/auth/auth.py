d93870f41bc8e968768909bc08235fb2ad1b592b
PrimeZ <the.primez.dev@gmail.com>
2025-08-16 13:08:13 +0330

    feat: added authorizations

---
+++ backend/auth/auth.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from db.engine import get_db
+from config import config
+from db import crud
+
+
+SECRET_KEY = "njQdMj0mCjW1TmFHx4eXlvdp3m7Klbtr3wFs0+5Rra9w4Fahd0U2R9Ezorh3SGu2"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+router = APIRouter(tags=["Login"])
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    main_admin_username = config.ADMIN_USERNAME
+    main_admin_password = config.ADMIN_PASSWORD
+    if username == main_admin_username and password == main_admin_password:
+        return {"username": username, "type": "main_admin"}
+
+    admin = crud.it_is_admin(db, username=username)
+    if admin:
+        if verify_password(password, admin.password):
+            return {"username": admin.username, "type": "admin"}
+
+    return None
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+@router.post("/login")
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    admin = authenticate_user(db, form_data.username, form_data.password)
+    if not admin:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="The username or password is incorrect",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": admin["username"], "role": admin["type"]},
+        expires_delta=access_token_expires,
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_type: str = payload.get("type")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    return {"username": username, "type": user_type}
