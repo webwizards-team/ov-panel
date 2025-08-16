b0acf71ba97f32e4d18d95b728f6592f8343c50b
PrimeZ <the.primez.dev@gmail.com>
2025-08-16 13:09:30 +0330

    improved config names

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,15 @@
+import os
 from pydantic_settings import BaseSettings
 
 
 class Setting(BaseSettings):
-    USERNAME: str
-    PASSWORD: str
+    ADMIN_USERNAME: str
+    ADMIN_PASSWORD: str
     URLPATH: str
     DEBUG: str
 
     class Config:
-        env_file = "../.env"
+        env_file = os.path.join(os.path.dirname(__file__), "..", ".env")
 
 
 config = Setting()
