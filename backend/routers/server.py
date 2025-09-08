55d085cdb6276b0bdfb7b35fef08cbb035bbcaea
PrimeZ <the.primez.dev@gmail.com>
2025-09-08 13:17:27 +0330

    feat: added server informations API

--- backend/routers/server.py
+++ backend/routers/server.py
@@ -1,12 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+
 from schema.output import ServerInfo
 from operations.server_info import get_server_info
+from auth.auth import get_current_user
 
 
 router = APIRouter(prefix="/server", tags=["Server info"])
 
 
 @router.get("/info", response_model=ServerInfo)
-async def get_server_information():
+async def get_server_information(user: dict = Depends(get_current_user)):
     result = await get_server_info()
     return result
